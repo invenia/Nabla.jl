040f0fe6a533cefd2e3d4a40bbb9d3d7bd418547
[A[A[A[B[B[Bwilltebbutt <wt0881@my.bristol.ac.uk>
2017-07-19 21:38:51 +0100

    Removed superfluous method from reducedim.jl

--- src/sensitivities/functional/reducedim.jl
+++ src/sensitivities/functional/reducedim.jl
@@ -1,8 +1,8 @@
 export mapreducedim, reducedim, sum, prod
 
 # Intercepts and sensitivities for mapreducedim.
-accepted_wo_default = :(Tuple{Function, typeof(+), AbstractArray{T} where T<:Real, Any})
-accepted_w_default = :(Tuple{Function, typeof(+), AbstractArray{T} where T<:Real, Any, Real})
+accepted_wo_default = :(Tuple{Function, typeof(+), AbstractArray{<:Real}, Any})
+accepted_w_default = :(Tuple{Function, typeof(+), AbstractArray{<:Real}, Any, Real})
 eval(DiffBase, add_intercept(:mapreducedim, :(Base.mapreducedim), accepted_wo_default))
 eval(DiffBase, add_intercept(:mapreducedim, :(Base.mapreducedim), accepted_w_default))
 
@@ -12,8 +12,9 @@ function ∇(
     ::Type{Arg{3}},
     p, y, ȳ, f,
     ::typeof(+),
-    A::AbstractArray{T} where T<:Real,
+    A::AbstractArray{<:Real},
     region,
+    v0=nothing,
 )
     if needs_output(f) && method_exists(∇, Tuple{typeof(f), Type{Arg{1}}, Real, Any})
         return Base.broadcast((An, ȳn)->ȳn * ∇(f, Arg{1}, An, f(An)), A, ȳ)
@@ -24,34 +25,13 @@ function ∇(
     end
 end
 
-# Sensitivity w.r.t. mapreducedim with default argument.
-function ∇(
-    ::typeof(mapreducedim),
-    ::Type{Arg{3}},
-    p, y, ȳ, f,
-    ::typeof(+),
-    A::AbstractArray{T} where T<:Real,
-    region,
-    v0::Real,
-)
-    if needs_output(f) && method_exists(∇, Tuple{typeof(f), Type{Arg{1}}, Real, Any})
-        return Base.broadcast((An, ȳn)->ȳn * ∇(f, Arg{1}, An, f(An)), A, ȳ)
-    elseif !needs_output(f) && method_exists(∇, Tuple{typeof(f), Type{Arg{1}}, Real})
-        return Base.broadcast((An, ȳn)->ȳn * ∇(f, Arg{1}, An), A, ȳ)
-    else
-        return Base.broadcast((An, ȳn)->ȳn * fmad(f, (An,), Val{1}), A, ȳn)
-    end
-end
-
-# Implementation of `reducedim` to avoid going into `Base`. Copied directly from `Base`.
-const A_type = Union{AbstractArray, Node{T} where T<:AbstractArray}
+# Implementation of `reducedim`, `sum` and `prod` to avoid going into `Base`.
+# Copied directly from `Base`.
+const A_type = Union{AbstractArray, Node{<:AbstractArray}}
 reducedim(op, A::A_type, region, v0) = mapreducedim(identity, op, A, region, v0)
 reducedim(op, A::A_type, region) = mapreducedim(identity, op, A, region)
-prevent_base_import(:reducedim)
-
-# Implementation of `sum` and `prod` to avoid going into `Base.` Copied directly from `Base`.
 sum(f::Function, A::A_type, region) = mapreducedim(f, +, A, region)
 sum(A::A_type, region) = sum(identity, A, region)
 prod(f::Function, A::A_type, region) = mapreducedim(f, *, A, region)
 prod(A::A_type, region) = prod(identity, A, region)
-prevent_base_import.((:sum, :prod))
+prevent_base_import.((:reducedim, :sum, :prod))
