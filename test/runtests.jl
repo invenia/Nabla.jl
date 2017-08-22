3043f2c15de3af9b3d9b1cf96086a8a3e6f94c20
Will Tebbutt <wt0881@my.bristol.ac.uk>
2017-08-22 19:10:31 +0100

    Uncomment unit tests
    Most of the unit tests had been commented out by accident.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,30 +1,30 @@
 using Base.Test, Nabla, Distributions
 
-# @testset "Core" begin
-#     include("core.jl")
-#     include("code_transformation/util.jl")
-#     include("code_transformation/differentiable.jl")
-#     include("sensitivity.jl")
-# end
+@testset "Core" begin
+    include("core.jl")
+    include("code_transformation/util.jl")
+    include("code_transformation/differentiable.jl")
+    include("sensitivity.jl")
+end
 
 @testset "Sensitivities" begin
 
-    # include("finite_differencing.jl")
+    include("finite_differencing.jl")
 
-    # # Test sensitivities for the basics.
-    # include("sensitivities/indexing.jl")
-    # include("sensitivities/scalar.jl")
+    # Test sensitivities for the basics.
+    include("sensitivities/indexing.jl")
+    include("sensitivities/scalar.jl")
 
-    # # Test sensitivities for functionals.
-    # include("sensitivities/functional/functional.jl")
-    # include("sensitivities/functional/reduce.jl")
-    # include("sensitivities/functional/reducedim.jl")
+    # Test sensitivities for functionals.
+    include("sensitivities/functional/functional.jl")
+    include("sensitivities/functional/reduce.jl")
+    include("sensitivities/functional/reducedim.jl")
 
-    # # Test sensitivities for linear algebra optimisations.
-    # include("sensitivities/linalg/generic.jl")
-    # # include("sensitivities/linalg/uniformscaling.jl")
-    # # include("sensitivities/linalg/diagonal.jl")
-    # # include("sensitivities/linalg/triangular.jl")
-    # include("sensitivities/linalg/strided.jl")
+    # Test sensitivities for linear algebra optimisations.
+    include("sensitivities/linalg/generic.jl")
+    # include("sensitivities/linalg/uniformscaling.jl")
+    # include("sensitivities/linalg/diagonal.jl")
+    # include("sensitivities/linalg/triangular.jl")
+    include("sensitivities/linalg/strided.jl")
     include("sensitivities/linalg/blas.jl")
 end
