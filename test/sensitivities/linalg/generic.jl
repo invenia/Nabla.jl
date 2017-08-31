08d9a55c3b16a1154bdbb8b41a1a2197514e688e
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2017-08-31 18:10:33 +0100

    Simpilified generation of random quantities
    The way random quantities for test cases were generated seemed to break
    the 32 bit build, so this is a simpler way that hopefully works.

--- test/sensitivities/linalg/generic.jl
+++ test/sensitivities/linalg/generic.jl
@@ -3,11 +3,11 @@
     let ϵ_abs = 1e-6, c_rel = 1e6, N = 5, rng = MersenneTwister(123456)
 
         # Generate random test quantities for specific types.
-        for f in [:rand, :randn]
-            @eval Base.$f(rng::AbstractRNG, ::Type{∇Real}, N::Int64) = $f(rng)
-            @eval Base.$f(rng::AbstractRNG, ::Type{∇RealArray}, N::Int64) = $f(rng, N, N)
-            @eval Base.$f(rng::AbstractRNG, ::Type{ArrayOr∇Real}, N::Int64) = $f(rng, ∇RealArray, N)
-        end
+        ∇Arrays = Union{Type{∇RealArray}, Type{ArrayOr∇Real}}
+        trandn(rng::AbstractRNG, ::∇Arrays) = randn(rng, N, N)
+        trandn(rng::AbstractRNG, ::Type{∇Real}) = randn(rng)
+        trand(rng::AbstractRNG, ::∇Arrays) = rand(rng, N, N)
+        trand(rng::AbstractRNG, ::Type{∇Real}) = rand(rng)
 
         # Get the identity associated to an object.
         id(::Any) = I
@@ -15,15 +15,15 @@
 
         # Test unary linalg sensitivities.
         for (f, T_In, T_Out, X̄, bounds) in Nabla.unary_linalg_optimisations
-            Z = rand(rng, T_In, N) .* (bounds[2] - bounds[1]) + bounds[1]
+            Z = trand(rng, T_In) .* (bounds[2] - bounds[1]) + bounds[1]
             X = Z'Z + 1e-6id(Z)
-            Ȳ, V = eval(f)(X), 1e-3 * randn(rng, T_In, N)
+            Ȳ, V = eval(f)(X), 1e-3 * trandn(rng, T_In)
             @test check_errs(eval(f), Ȳ, X, V'V + 1e-6id(V), ϵ_abs, c_rel)
         end
 
         # Test binary linalg sensitivities.
         for (f, T_A, T_B, T_Y, Ā, B̄) in Nabla.binary_linalg_optimisations
-            A, B, VA, VB = randn.(rng, (T_A, T_B, T_A, T_B), N)
+            A, B, VA, VB = trandn.(rng, (T_A, T_B, T_A, T_B))
             @test check_errs(eval(f), eval(f)(A, B), (A, B), 1e-6 .* (VA, VB), ϵ_abs, c_rel)
         end
     end
