2935e56aa624b909325af604b45d93cd1e2860f3
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2017-08-31 19:45:44 +0100

    Sensitivity for `reshape`

--- src/sensitivities/array.jl
+++ src/sensitivities/array.jl
@@ -1,3 +1,9 @@
 # Let the user get the `size` and `length` of `Node`s.
 Base.size(x::Node, dims...) = size(x.val, dims...)
 Base.length(x::Node) = length(x.val)
+
+# Sensitivity for the first argument of `reshape`.
+import Base.reshape
+@explicit_intercepts reshape Tuple{∇RealArray, Vararg{Int64}} [true, false]
+Nabla.∇(::typeof(reshape), ::Type{Arg{1}}, _, y, ȳ, A::∇RealArray, args...) =
+    reshape(ȳ, size(A)...)
--- test/sensitivities/array.jl
+++ test/sensitivities/array.jl
@@ -1,4 +1,11 @@
-@testset "array" begin
+@testset "sensitivities/array" begin
     @test size(Leaf(Tape(), ones(1, 2, 3, 4))) == (1, 2, 3, 4)
     @test length(Leaf(Tape(), 1:3)) == 3
+
+    let c_rel = 1e4, ε_abs = 1e-16, rng = MersenneTwister(123456)
+        x = randn(2, 10)
+        A = randn(4, 5)
+        f = x̂ -> reshape(x̂, 5, 4) * A
+        @test check_errs(f, f(x), (x,), (1e-6 * randn(size(x)...),), ε_abs, c_rel)
+    end
 end
