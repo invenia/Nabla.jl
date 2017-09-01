a6cd9ca1d3853bdc056612446e34f9a0195584ed
Wessel <wessel.p.bruinsma@gmail.com>
2017-09-01 11:36:53 +0200

    Sensitivity for `reshape` (#58)
    * Implement size and length of Nodes
    
    * Sensitivity for `reshape`
    
    * Stylistic changes and extended support for reshape
    
    * Changed Int64 to Int

--- src/sensitivities/array.jl
+++ src/sensitivities/array.jl
@@ -1,3 +1,11 @@
+import Base: size, length, reshape
+
 # Let the user get the `size` and `length` of `Node`s.
 Base.size(x::Node, dims...) = size(x.val, dims...)
 Base.length(x::Node) = length(x.val)
+
+# Sensitivity for the first argument of `reshape`.
+@explicit_intercepts reshape Tuple{∇RealArray, Vararg{Int}} [true, false]
+@explicit_intercepts reshape Tuple{∇RealArray, Tuple{Vararg{Int}}} [true, false]
+∇(::typeof(reshape), ::Type{Arg{1}}, _, y, ȳ, A::∇RealArray, args...) =
+    reshape(ȳ, size(A)...)
