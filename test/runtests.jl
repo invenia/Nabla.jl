d908412621401ca3be8ad41f25d403dac96ab06d
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2017-09-02 16:00:25 +0100

    Uncommented commented unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,32 +1,32 @@
 using Base.Test, Nabla, Distributions
 
-# @testset "Core" begin
-#     include("core.jl")
-#     include("code_transformation/util.jl")
-#     include("code_transformation/differentiable.jl")
-#     include("sensitivity.jl")
-# end
+@testset "Core" begin
+    include("core.jl")
+    include("code_transformation/util.jl")
+    include("code_transformation/differentiable.jl")
+    include("sensitivity.jl")
+end
 
 @testset "Sensitivities" begin
 
     include("finite_differencing.jl")
-    #
-    # # Test sensitivities for the basics.
-    # include("sensitivities/indexing.jl")
-    # include("sensitivities/scalar.jl")
-    # include("sensitivities/array.jl")
-    #
-    # # Test sensitivities for functionals.
-    # include("sensitivities/functional/functional.jl")
-    # include("sensitivities/functional/reduce.jl")
-    # include("sensitivities/functional/reducedim.jl")
-    #
-    # # Test sensitivities for linear algebra optimisations.
-    # include("sensitivities/linalg/generic.jl")
-    # # include("sensitivities/linalg/uniformscaling.jl")
-    # include("sensitivities/linalg/diagonal.jl")
-    # include("sensitivities/linalg/triangular.jl")
-    # include("sensitivities/linalg/strided.jl")
-    # include("sensitivities/linalg/blas.jl")
-    # include("sensitivities/linalg/factorization/cholesky.jl")
+
+    # Test sensitivities for the basics.
+    include("sensitivities/indexing.jl")
+    include("sensitivities/scalar.jl")
+    include("sensitivities/array.jl")
+
+    # Test sensitivities for functionals.
+    include("sensitivities/functional/functional.jl")
+    include("sensitivities/functional/reduce.jl")
+    include("sensitivities/functional/reducedim.jl")
+
+    # Test sensitivities for linear algebra optimisations.
+    include("sensitivities/linalg/generic.jl")
+    # include("sensitivities/linalg/uniformscaling.jl")
+    include("sensitivities/linalg/diagonal.jl")
+    include("sensitivities/linalg/triangular.jl")
+    include("sensitivities/linalg/strided.jl")
+    include("sensitivities/linalg/blas.jl")
+    include("sensitivities/linalg/factorization/cholesky.jl")
 end
