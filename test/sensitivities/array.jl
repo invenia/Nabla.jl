1291f9615997fed2ca91ebd5a035abc193059080
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2017-09-02 16:18:05 +0100

    Reduced tolerance of array.jl unit tests

--- test/sensitivities/array.jl
+++ test/sensitivities/array.jl
@@ -2,7 +2,7 @@
     @test size(Leaf(Tape(), ones(1, 2, 3, 4))) == (1, 2, 3, 4)
     @test length(Leaf(Tape(), 1:3)) == 3
 
-    let c_rel = 1e4, ε_abs = 1e-16, rng = MersenneTwister(123456)
+    let c_rel = 1e6, ε_abs = 1e-16, rng = MersenneTwister(123456)
         x = randn(2, 10)
         f1 = x̂ -> reshape(x̂, 5, 4)
         f2 = x̂ -> reshape(x̂, (5, 4))
