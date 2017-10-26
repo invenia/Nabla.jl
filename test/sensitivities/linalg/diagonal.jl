d87f9da433365504709ac2b52b0fbb9017592068
Wessel <wessel.p.bruinsma@gmail.com>
2017-10-26 13:27:39 +0100

    Sensitivity for `diag` (#69)
    * added sensitivity for diag
    
    * Fixed the in-place sensitivities for diag
    
    x̄ had a wrong type, and the sensitivities returned the diagonal of what they
    should have returned instead.

--- test/sensitivities/linalg/diagonal.jl
+++ test/sensitivities/linalg/diagonal.jl
@@ -24,6 +24,18 @@
     end
     let rng = MersenneTwister(123456), N = 10
         for _ in 1:10
+            A = randn(rng, N, N)
+            VA = randn(rng, N, N)
+            @test check_errs(x -> diag(x), randn(rng, N), A, VA)
+
+            # Check various diagonals.
+            for k = -3:3
+                @test check_errs(x -> diag(x, k), randn(rng, N - abs(k)), A, VA)
+            end
+        end
+    end
+    let rng = MersenneTwister(123456), N = 10
+        for _ in 1:10
             A = randn(rng, N)
             VA = randn(rng, N)
             @test check_errs(Diagonal, Diagonal(randn(rng, N)), A, VA)
