316a004ea46e8e1648adafd3dad60c25119b4690
[A[A[A[B[B[Bwilltebbutt <wt0881@my.bristol.ac.uk>
2018-01-12 16:31:51 +0000

    another tmp commit

--- src/sensitivities/scalar.jl
+++ src/sensitivities/scalar.jl
@@ -29,7 +29,6 @@ unary_sensitivities, binary_sensitivities = [], []
 for (package, f, arity) in diffrules()
     (package == :NaNMath || (package, f) in ignored_fs) && continue
 
-    eval(:import $package.$f)
     @eval import $package.$f
     if arity == 1
         push!(unary_sensitivities, (package, f))
