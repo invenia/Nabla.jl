6c933503603eb0780a0458fdcee53ca52a263718
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-01-17 14:51:10 +0000

    removed full and simplified type assertions

--- src/sensitivities/linalg/symmetric.jl
+++ src/sensitivities/linalg/symmetric.jl
@@ -1,4 +1,4 @@
 import Base.Symmetric
 @explicit_intercepts Symmetric Tuple{∇Array}
 ∇(::typeof(Symmetric), ::Type{Arg{1}}, p, Y::∇Array, Ȳ::∇Array, X::∇Array) =
-    full(UpperTriangular(Ȳ) + LowerTriangular(Ȳ)') - Diagonal(Ȳ)
+    UpperTriangular(Ȳ) + LowerTriangular(Ȳ)' - Diagonal(Ȳ)
