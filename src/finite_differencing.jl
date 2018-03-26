4335a351cdb1c8b3e611493fdef569545eb2b47f
Wessel <wessel.p.bruinsma@gmail.com>
2018-03-26 15:37:48 +0100

    Integration with FDM.jl (#91)
    * separated fd functionality
    
    * integration with FDM.jl
    
    * remove FDM tests
    
    * remove call to FDM tests
    
    * added FDM.jl to REQUIRE

--- src/finite_differencing.jl
+++ src/finite_differencing.jl
@@ -1,5 +1,7 @@
+using FDM
+
 export check_Dv, check_Dv_update, check_errs, fdm, forward_fdm, central_fdm, backward_fdm,
-       check_approx_equal, domain1, domain2, points, in_domain
+       assert_approx_equal, domain1, domain2, points, in_domain
 
 """
     approximate_Dv(
@@ -21,8 +23,10 @@ function approximate_Dv(
 )
     return central_5_1(ε -> sum(ȳ .* f((x .+ ε .* v)...)))
 end
+central_5_1 = central_fdm(5, 1; M=5e8)
 approximate_Dv(f, ȳ::∇ArrayOrScalar, x::∇ArrayOrScalar, v::∇ArrayOrScalar) =
     approximate_Dv(f, ȳ, (x,), (v,))
+
 
 """
     compute_Dv(
@@ -110,155 +114,10 @@ function check_errs(
     ∇x_alloc = compute_Dv(f, ȳ, x, v)
     ∇x_inplace = compute_Dv_update(f, ȳ, x, v)
     ∇x_fin_diff = approximate_Dv(f, ȳ, x, v)
-    return check_approx_equal("<$f> allocated at $x", ∇x_alloc, ∇x_fin_diff, ε_abs, ε_rel) &
-           check_approx_equal("<$f> in-place at $x", ∇x_inplace, ∇x_fin_diff, ε_abs, ε_rel)
-end
-
-function check_approx_equal(desc, x, y, ε_abs, ε_rel)
-    if abs(x - y) >= ε_abs + ε_rel * (abs(x) + abs(y))
-        msg = "\"$desc\": large deviation from reference:\n" *
-              "  relative error:  $(@sprintf "%.3e" abs(x - y) / (abs(x) + abs(y)))\n" *
-              "    tolerance:     $(@sprintf "%.3e" ε_rel)\n" *
-              "  absolute error:  $(@sprintf "%.3e" abs(x - y))\n" *
-              "    tolerance:     $(@sprintf "%.3e" ε_abs)\n"
-        throw(ErrorException(msg))
-    end
+    assert_approx_equal(∇x_alloc, ∇x_fin_diff, ε_abs, ε_rel, "<$f> allocated")
+    assert_approx_equal(∇x_inplace, ∇x_fin_diff, ε_abs, ε_rel, "<$f> in-place")
     return true
 end
-
-"""
-    FDMReport
-
-Details of a finite-difference method to estimate a derivative. Instances of `FDMReport`
-`Base.show` nicely.
-
-# Fields
-- `p::Int`: Order of the method.
-- `q::Int`: Order of the derivative that is estimated.
-- `grid::Vector{<:∇Scalar}`: Relative spacing of samples of `f` that are used by the method.
-- `coefs::Vector{<:∇Scalar}`: Weights of the samples of `f`.
-- `ε::∇Scalar`: Absolute roundoff error of the function evaluations.
-- `M::∇Scalar`: Assumed upper bound of `f` and all its derivatives at `x`.
-- `ĥ::∇Scalar`: Step size.
-- `err::∇Scalar`: Estimated absolute accuracy.
-"""
-struct FDMReport
-    p::Int
-    q::Int
-    grid::Vector{<:∇Scalar}
-    coefs::Vector{<:∇Scalar}
-    ε::∇Scalar
-    M::∇Scalar
-    ĥ::∇Scalar
-    acc::∇Scalar
-end
-function Base.show(io::IO, x::FDMReport)
-    @printf io "FDMReport:\n"
-    @printf io "  order of method:       %d\n" x.p
-    @printf io "  order of derivative:   %d\n" x.q
-    @printf io "  grid:                  %s\n" x.grid
-    @printf io "  coefficients:          %s\n" x.coefs
-    @printf io "  roundoff error:        %.2e\n" x.ε
-    @printf io "  bounds on derivatives: %.2e\n" x.M
-    @printf io "  step size:             %.2e\n" x.ĥ
-    @printf io "  accuracy:              %.2e\n" x.acc
-end
-
-"""
-    function fdm(
-        grid::Vector{<:∇Scalar},
-        q::Int;
-        ε::∇Scalar=eps(),
-        M::∇Scalar=5e8,
-        report::Bool=false
-    )
-
-Construct a function `method(f::Function, x::∇Scalar, h::∇Scalar=ĥ)` that takes in a
-function `f`, a point `x` in the domain of `f`, and optionally a step size `h`, and
-estimates the `q`'th order derivative of `f` at `x` with a `length(grid)`'th order
-finite-difference method.
-
-# Arguments
-- `grid::Vector{<:∇Scalar}`: Relative spacing of samples of `f` that are used by the method.
-    The length of `grid` determines the order of the method.
-- `q::Int`: Order of the derivative to estimate. `q` must be strictly less than the order
-    of the method.
-
-# Keywords
-- `ε::∇Scalar=eps()`: Absolute roundoff error of the function evaluations.
-- `M::∇Scalar=5e8`: Assumed upper bound of `f` and all its derivatives at `x`.
-- `report::Bool=false`: Also return an instance of `FDMReport` containing information
-    about the method constructed.
-"""
-function fdm(
-    grid::Vector{<:∇Scalar},
-    q::Int;
-    ε::∇Scalar=eps(),
-    M::∇Scalar=5e8,
-    report::Bool=false
-)
-    p = length(grid)  # Order of the method.
-    q < p || throw(ArgumentError("Order of the method must be strictly greater than that " *
-                                 "of the derivative."))
-
-    # Check whether the method can be computed.
-    try
-        factorial(p)
-    catch e
-        isa(e, OverflowError) && throw(ArgumentError("Order of the method is too large " *
-                                                     "to be computed."))
-    end
-
-    # Compute the coefficients of the FDM.
-    C = hcat([grid.^i for i = 0:p - 1]...)'
-    x = [i == q + 1 ? factorial(q) : 0 for i = 1:p]
-    coefs = C \ x
-
-    # Set the step size optimally by minimising an upper bound on the total error of the
-    # estimate.
-    C₁ = ε * sum(abs.(coefs))
-    C₂ = M * sum(abs.(coefs .* grid.^p)) / factorial(p)
-    ĥ = (q / (p - q) * C₁ / C₂) .^ (1 / p)
-
-    # Compute the accuracy of the method.
-    acc = ĥ^(-q) * C₁ + ĥ^(p - q) * C₂
-
-    # Construct the FDM.
-    method(f::Function, x::∇Scalar=0, h::∇Scalar=ĥ) = sum(coefs .* f.(x .+ h .* grid)) / h^q
-
-    # If asked for, also return information.
-    if report
-        return method, FDMReport(p, q, grid, coefs, ε, M, ĥ, acc)
-    else
-        return method
-    end
-end
-fdm(grid::UnitRange{Int}, args...; kws...) = fdm(Array(grid), args...; kws...)
-
-"""
-    backward_fdm(p::Int, ...)
-    forward_fdm(p::Int, ...)
-    central_fdm(p::Int, ...)
-
-Construct a backward, forward, or central finite-difference method of order `p`. See `fdm`
-for further details.
-
-# Arguments
-- `p::Int`: Order of the method.
-
-Further takes, in the following order, the arguments `q`, `ε`, `M`, and `report` from `fdm`.
-"""
-backward_fdm(p::Int, args...; kws...) = fdm(1 - p:0, args...; kws...)
-forward_fdm(p::Int, args...; kws...) = fdm(0:p - 1, args...; kws...)
-function central_fdm(p::Int, args...; kws...)
-    return isodd(p) ? fdm(Int(-(p - 1) / 2):Int((p - 1) / 2), args...; kws...) :
-                      fdm([Int(-p/2):-1; 1:Int(p/2)], args...; kws...)
-end
-
-# Precompute some FDMs.
-central_3_1 = central_fdm(3, 1)
-central_5_1 = central_fdm(5, 1)
-central_7_1 = central_fdm(7, 1)
 
 """
     in_domain(f::Function, x::Float64...)
@@ -273,8 +132,6 @@ function in_domain(f::Function, x::Float64...)
         return isa(err, DomainError) ? false : throw(err)
     end
 end
-
-
 
 # Test points that are used to determine functions's domains.
 points = [-π + .1, -.5π + .1, -.9, -.1, .1, .9, .5π - .1, π - .1]
