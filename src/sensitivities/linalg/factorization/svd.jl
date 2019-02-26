6fd4f8291ae133a1c72bdfcacd008a02e3e1b844
Alex Arslan <ararslan@comcast.net>
2019-02-26 10:45:37 -0800

    Allow destructuring SVD objects in Branches
    This defines `Base.iterate(::Branch{<:SVD})` to allow the common pattern
    of `U, S, V = svd(A)`.

--- src/sensitivities/linalg/factorization/svd.jl
+++ src/sensitivities/linalg/factorization/svd.jl
@@ -28,6 +28,14 @@ function ∇(::typeof(getproperty), ::Type{Arg{1}}, p, y, ȳ, USV::SVD, x::Symb
     end
 end
 
+# Iteration allows destructuring, e.g. U, S, V = svd(A)
+# These definitions mirror those defined in the LinearAlgebra module, see
+# https://github.com/JuliaLang/julia/blob/master/stdlib/LinearAlgebra/src/svd.jl#L20-L24
+Base.iterate(usv::Branch{<:SVD}) = (usv.U, Val(:S))
+Base.iterate(usv::Branch{<:SVD}, ::Val{:S}) = (usv.S, Val(:V))
+Base.iterate(usv::Branch{<:SVD}, ::Val{:V}) = (usv.V, Val(:done))
+Base.iterate(usv::Branch{<:SVD}, ::Val{:done}) = nothing
+
 """
     svd_rev(USV, Ū, S̄, V̄)
 
