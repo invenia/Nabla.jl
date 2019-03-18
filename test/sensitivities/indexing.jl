b62038b0bec92886ab2f3de32e66e540ed0cee08
Will Tebbutt <wt0881@my.bristol.ac.uk>
2019-03-18 13:12:36 +0000

    Fix indexing + add regression test

--- test/sensitivities/indexing.jl
+++ test/sensitivities/indexing.jl
@@ -1,15 +1,22 @@
 @testset "Indexing" begin
-    let
+    @testset "Int" begin
         leaf = Leaf(Tape(), 5 * [1, 1, 1, 1, 1])
         y = getindex(leaf, 1)
         @test unbox(y) == 5
         @test ∇(y, one(unbox(y)))[leaf] == [1, 0, 0, 0, 0]
     end
 
-    let
+    @testset "Vector" begin
         x = Leaf(Tape(), 10 * [1, 1, 1])
         y = x[2:3]
         @test unbox(y) == [10, 10]
         @test ∇(y, oneslike(unbox(y)))[x] == [0, 1, 1]
+    end
+
+    @testset "Overlapping indices (#139)" begin
+        x = Leaf(Tape(), 10 * [1, 1, 1])
+        y = x[[2, 3, 3]]
+        @test unbox(y) == [10, 10, 10]
+        @test ∇(y, oneslike(unbox(y)))[x] == [0, 1, 2]
     end
 end
