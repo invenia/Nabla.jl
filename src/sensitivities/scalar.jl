2f2e7f0fc96ca7b04e3b2055f84e6891ce5e79c2
Alex Arslan <ararslan@comcast.net>
2019-03-22 09:43:52 -0700

    Add sensitivities for float
    `float` acts on arrays and scalars, turning integers into floats of the
    corresponding size (e.g. `float(::Int32)` -> `Float32`) and is a no-op
    for floats. This same behavior is applied when differentiating.

--- src/sensitivities/scalar.jl
+++ src/sensitivities/scalar.jl
@@ -44,3 +44,7 @@ end
 
 # Add method to resolve exponentiation ambiguity.
 ^(n::Node{<:Real}, p::Integer) = invoke(^, Tuple{Node{<:Real}, Real}, n, p)
+
+import Base: float
+@explicit_intercepts float Tuple{∇ArrayOrScalar}
+∇(::typeof(float), ::Type{Arg{1}}, p, y, ȳ, x) = float(ȳ)
