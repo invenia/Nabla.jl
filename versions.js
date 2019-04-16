9a9a0d964ae9b7bdea661c7e3946b3be5da8273d
autodocs <autodocs>
2019-04-16 22:08:00 +0000

    build based on f151a55

--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.11",
   "release-0.10",
   "release-0.9",
   "release-0.8",
@@ -9,6 +10,7 @@ var DOC_VERSIONS = [
   "release-0.3",
   "release-0.2",
   "release-0.1",
+  "v0.11.0",
   "v0.10.0",
   "v0.9.0",
   "v0.8.0",
