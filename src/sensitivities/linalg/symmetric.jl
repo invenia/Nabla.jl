b71b6d02e97f4ea8b5c6da0f249f6ffcee7ad84d
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-10-12 16:29:11 +0100

    Correct ∇ for Symmetric constructor

--- src/sensitivities/linalg/symmetric.jl
+++ src/sensitivities/linalg/symmetric.jl
@@ -1,4 +1,4 @@
 import LinearAlgebra: Symmetric
 @explicit_intercepts Symmetric Tuple{∇Array}
-∇(::typeof(Symmetric), ::Type{Arg{1}}, p, Y::∇Array, Ȳ::∇Array, X::∇Array) =
+∇(::Type{Symmetric}, ::Type{Arg{1}}, p, Y::∇Array, Ȳ::∇Array, X::∇Array) =
     UpperTriangular(Ȳ) + LowerTriangular(Ȳ)' - Diagonal(Ȳ)
