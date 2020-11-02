ff44f9c0984a4425c3b7245d97342343a347ce9b
Lyndon White <oxinabox@ucc.asn.au>
2020-11-02 16:41:05 +0000

    Apply suggestions from code review

--- test/code_transformation/differentiable.jl
+++ test/code_transformation/differentiable.jl
@@ -63,7 +63,7 @@ skip_line_info(ex) = ex
     @test unionise_sig(:(foo(x::T))) == :(foo($(unionise_arg(:(x::T)))))
     @test unionise_sig(:(foo(x::T) where T)) == :(foo($(unionise_arg(:(x::T)))) where T)
 
-    @test isequal(  # special case for a redudant where N in a Vararg
+    @test isequal(  # special case for a redudant `where N` in a Vararg
         Nabla.unionise_sig(:(x2::(Vararg{Int64, N} where N))),
         :(x2::Vararg{Union{Int64, Node{<:Int64}}}),
     )
