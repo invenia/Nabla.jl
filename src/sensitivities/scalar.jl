5151ad89f2298dc4374e6ca3e8ef917759b30d08
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-11-13 14:27:50 +0000

    Delete redundant rule for identity

--- src/sensitivities/scalar.jl
+++ src/sensitivities/scalar.jl
@@ -1,11 +1,3 @@
-# Hand code the identity because it's really fundamental. It doesn't need to generate a new
-# node on the computational graph since it does nothing, but it is useful to have it's
-# gradient implemented for use in higher-order functions.
-import Base.identity
-@explicit_intercepts identity Tuple{Any}
-@inline ∇(::typeof(identity), ::Type{Arg{1}}, p, y, ȳ, x) = ȳ
-@inline ∇(::typeof(identity), ::Type{Arg{1}}, x::Real) = one(x)
-
 # Add method to resolve exponentiation ambiguity.
 ^(n::Node{<:Real}, p::Integer) = invoke(^, Tuple{Node{<:Real}, Real}, n, p)
 
