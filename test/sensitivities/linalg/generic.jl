64502d19b3025f6055921d5b065c074e8e0711e0
Lyndon White <oxinabox@ucc.asn.au>
2021-06-25 11:44:14 +0100

    Change test to reflect that asymmetric matrix expodential now works

--- test/sensitivities/linalg/generic.jl
+++ test/sensitivities/linalg/generic.jl
@@ -111,12 +111,16 @@
     @testset "exp" begin
         rng = MersenneTwister(12345)
         n = 10
-        symm!(X) = (X .= (X .+ X') ./ 2; X)
-        X = symm!(randn(rng, n, n))
-        VX = symm!(randn(rng, n, n))
-        @test check_errs(exp, randn(rng, n, n), X, VX)
-        A = randn(rng, n, n)
-        VA = randn(rng, n, n)
-        @test_throws ArgumentError check_errs(exp, randn(rng, n, n), A, VA)
+        @testset "Symmetric" begin
+            symm!(X) = (X .= (X .+ X') ./ 2; X)
+            X = symm!(randn(rng, n, n))
+            VX = symm!(randn(rng, n, n))
+            @test check_errs(exp, randn(rng, n, n), X, VX)
+        end
+        @testset "Asymmetric" begin
+            A = randn(rng, n, n)
+            VA = randn(rng, n, n)
+            @test check_errs(exp, randn(rng, n, n), A, VA)
+        end
     end
 end
