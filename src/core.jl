f4ab3a724f10e8ed030827d98274cff71b5516fc
bailey.shirtliff <bailey.shirtliff@invenia.ca>
2022-08-29 12:06:54 -0500

    Adds end to if statement on version check

--- src/core.jl
+++ src/core.jl
@@ -89,6 +89,7 @@ function Branch(f, args::Tuple, tape::Tape; kwargs...)
         branch = Branch(primal_val, f, args, values(kwargs), tape, length(tape) + 1, pullback)
     else
         branch = Branch(primal_val, f, args, kwargs.data, tape, length(tape) + 1, pullback)
+    end
     push!(tape, branch)
     return branch
 end
